797951bc98c8ad582786dca1f2fba91e2b81783f
이상영 <a27179820@gmail.com>
2026-06-21 17:12:22 +0900

    모바일2

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
-const CACHE_NAME = "falling-game-v1";
+const CACHE_NAME = "falling-game-v2";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css",
-  "./script.js",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
